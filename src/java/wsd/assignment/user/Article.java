45ba659d2453a9bdc3c4a5abbbdfc21e956e0bcd
vicky156 <weiyi156@gmail.com>
2013-10-29 20:57:37 +1100

    Update Article.java

--- src/java/wsd/assignment/user/Article.java
+++ src/java/wsd/assignment/user/Article.java
@@ -8,17 +8,23 @@ import java.io.Serializable;
 public class Article implements Serializable {
 	
 	private String title;
+	private int year;
 	private int date;
+	private int month;
+	private String body;
 	
 	public Article() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Article(String title, int date) {
+	public Article(String title, int date, int year, int month, String body) {
 		super();
 		this.title = title;
 		this.date = date;
+		this.year = year;
+		this.month = month;
+		this.body = body;
 
 	}
 
@@ -36,6 +42,36 @@ public class Article implements Serializable {
 
 	public void setDate(int date) {
 		this.date = date;
+	}
+	
+	
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	
+	@XmlElement(name = "year")
+	public int getYear() {
+		return year;
+	}
+	@XmlElement(name = "month")
+	public int getMonth() {
+		return month;
+	}
+	
+	public void setBody(String body {
+		this.body = body;
+	}
+	
+	@XmlElement(name = "body")
+	public String getBody() {
+		return body;
 	}
 
 }
